372716be649832cdc60bb6347e7dd45fa03cc283
Rafał Burczyński <merfik18@gmail.com>
2016-01-02 14:46:59 +0100

    połączona minimapa z widokiem z pierwszej osoby, poprawiony drobny bug w minimapie

--- main.sh
+++ main.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# jeśli okno terminala nie ma zmieniać rozmiaru,
+# należy zakomentować tę linię
+printf "\e[8;21;39t"
+
 . maze.sh
 
 setmaze 20 50
@@ -25,6 +29,8 @@ for(( i=0; $i < 8; i++ )); do
     VIEWS[i]=$(cat "art/${i}.txt")
 done
 
+IFS=$'\n'
+
 draw() {
     x=0
     DIR_L=$[DIR-1]
@@ -34,7 +40,13 @@ draw() {
     [ "$(signAt $[PL_X+DX[DIR_L]] $[PL_Y+DY[DIR_L]])" = "$CH_FREE" ] && x=$[x|1]
     [ "$(signAt $[PL_X+DX[DIR]] $[PL_Y+DY[DIR]])" = "$CH_FREE" ]     && x=$[x|2]
     [ "$(signAt $[PL_X+DX[DIR_R]] $[PL_Y+DY[DIR_R]])" = "$CH_FREE" ] && x=$[x|4]
-    printf "%s\n" "${VIEWS[$x]}"
+    VIEW=(${VIEWS[x]})
+    MAP=($(minimap))
+    printf "%s\n" "${VIEWS[$x]}" | head -14
+    printf "%s+-------------+%s\n" ${VIEW[14]:0:12} ${VIEW[14]:27:12}
+    for ((i=0; $i < 5 ;i++)); do
+        printf "%s|%s|%s\n" ${VIEW[i+15]:0:12} ${MAP[i]} ${VIEW[i+15]:27:12}
+    done
 }
 
 # 13x5
@@ -49,7 +61,7 @@ minimap() {
     L_sp=""
     R_sp=""
     l=$[PL_Y-6]
-    r=$[PL_Y+6]
+    r=$[PL_Y+7]
     while [ $l -lt 0 ]; do
         L_sp+=" "
         ((l++))
@@ -77,30 +89,38 @@ minimap() {
     done
 }
 
-printmaze
+draw
+moves=0
 while read -sn 1 key; do
+    ((moves++))
     case $key in
-        k|K) echo "UPARR"
+        k|K)
             if [ "$(signAt $[PL_X+DX[DIR]] $[PL_Y+DY[DIR]])" = "$CH_FREE" ]; then
                 PL_X=$[PL_X+DX[DIR]]
                 PL_Y=$[PL_Y+DY[DIR]]
             fi
+            if [ $PL_X -eq 1 ] && [ $PL_Y -eq $[T_COLS-1] ]; then
+                draw
+                echo "Wygrana!"
+                echo "Labirynt pokonano w ${moves} ruchach."
+                exit 0
+            fi
             ;;
-        j|J) echo "DNARR"
+        j|J)
             DIR=$[DIR-2]
             [ $DIR -lt 0 ] && DIR=$[DIR+4]
             ;;
-        h|H) echo "LTARR"
+        h|H)
             DIR=$[DIR-1]
             [ $DIR -lt 0 ] && DIR=$[DIR+4]
             ;;
-        l|L) echo "RTARR"
+        l|L)
             DIR=$[DIR+1]
             [ $DIR -gt 3 ] && DIR=$[DIR-4]
             ;;
     esac
     draw
-    printmaze
-    minimap
+    #printmaze
+    #minimap
 done
 
