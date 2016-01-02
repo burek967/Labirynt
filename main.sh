5b617c63ece1d677114a97afae9e2501c52c1b11
Rafał Burczyński <merfik18@gmail.com>
2016-01-02 13:48:19 +0100

    można już rysować widok z pierwszej osoby

--- main.sh
+++ main.sh
@@ -20,6 +20,23 @@ CH_PL=("\u25B2" "\u25B6" "\u25BC" "\u25C0")
 DIR=0
 [ "$(signAt $PL_X 2)" = "$CH_FREE" ] && DIR=1
 
+draw() {
+    x=0
+    DIR_L=$[DIR-1]
+    DIR_R=$[DIR+1]
+    [ $DIR_L -lt 0 ] && DIR_L=$[DIR_L+4]
+    [ $DIR_R -gt 3 ] && DIR_R=$[DIR_R-4]
+    [ "$(signAt $[PL_X+DX[DIR_L]] $[PL_Y+DY[DIR_L]])" = "$CH_FREE" ] && x=$[x|1]
+    [ "$(signAt $[PL_X+DX[DIR]] $[PL_Y+DY[DIR]])" = "$CH_FREE" ]     && x=$[x|2]
+    [ "$(signAt $[PL_X+DX[DIR_R]] $[PL_Y+DY[DIR_R]])" = "$CH_FREE" ] && x=$[x|4]
+    cat "art/${x}.txt"
+}
+
+# 13x6
+minimap() {
+ # todo
+}
+
 printmaze
 while read -sn 1 key; do
     case $key in
@@ -42,6 +59,7 @@ while read -sn 1 key; do
             [ $DIR -gt 3 ] && DIR=$[DIR-4]
             ;;
     esac
+    draw
     printmaze
 done
 
